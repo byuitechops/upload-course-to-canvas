c5a476dc8ab3ad3a99828b1a07aa69f105502c16
Danverde <gre12014@byui.edu>
2017-05-30 10:04:43 -0600

    successfully begins the upload process

--- cli.js
+++ cli.js
@@ -4,34 +4,73 @@
 'use-strict';
 
 const https = require('https'),
+    chalk = require('chalk'),
     queryString = require('query-string'),
-    auth = require('./auth.js');
+    auth = require('./auth.js'),
+    chalk = require('chalk');
 
+
+function uploadZip(uploadParams) {
+    console.log('uploadParams', uploadParams);
+
+    var postBody = queryString.stringify({}),
+        postOptions = {
+
+        },
+        request = https.request(postOptions, function (response, err) {
+
+        });
+    request.on('error', function (err) {
+        console.log(chalk.red(err));
+    })
+
+}
 
 function createMigration() {
 
-    var post_body = queryString.stringify({
-            file_name: 'D2LExport_236812_201752517.zip',
-            size: 'size=34930210',
-            content_type: 'folder/zip'
+    var postBody = queryString.stringify({
+            migration_type: 'zip_file_importer',
+            'pre_attachment[name]': 'D2LExport_236812_201752517.zip',
+            'pre_attachment[size]': '34930210',
+            'pre_attachment[content_type]': 'folder/zip',
+            'settings[folder_id]': auth.parentFolderId
+
         }),
         postOptions = {
+            hostname: 'byui.instructure.com',
+            path: '/api/v1/courses/' + auth.courseId + '/content_migrations',
             method: 'POST',
-            path: '/api/v1/courses/' + auth.courseID + '/content_migrations',
-            hostname: 'https://byui.instructure.com',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(post_body)
+                Authorization: 'Bearer ' + auth.token
             }
         },
         request = https.request(postOptions, function (response, err) {
+            response.on('error', function (err) {
+                console.error(err);
+            });
+
+
             console.log(response.statusCode);
+
+            var data = '';
+            response.on('data', function (d) {
+                data += d;
+            });
+            response.on('end', function () {
+                data = JSON.parse(data);
+
+                //error handling here
+                uploadZip(data.pre_attachment.upload_params);
+
+            });
+
         });
+    request.end(postBody);
     request.on('error', function (err) {
         console.error(err);
     });
 
 }
-
 
 createMigration();
